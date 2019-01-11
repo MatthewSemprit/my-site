1e7e8c46ae9b04b8cf75d4a92ab9e7f1a87a9151
Matthew Semprit <semprit2008@gmail.com>
2019-01-11 00:58:56 -0500

    First commit, card, four divs.

---
+++ public/js/app.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+
+
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', '/js/particlesjs-config.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+console.log("load.js");
+
+});
