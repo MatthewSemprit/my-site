0de1980dad5a8985e0d1771dfb29f16a79db075d
Matthew Semprit <semprit2008@gmail.com>
2019-03-06 22:15:19 -0500

    fixed server

--- public/js/server.js
+++ public/js/server.js
@@ -2,14 +2,10 @@ const express = require('express');
 
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
 const port = 3000;
 
-app.use(express.static('../../../'));
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
+// app.use(express.static('../../../')); // for use when on matthewsemprit.github.io
+app.use(express.static('../../public/')); //for use localhost:3000
 
 http.listen(port, function(){
   console.log('listening on *:3000');
